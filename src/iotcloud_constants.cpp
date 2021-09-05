66e9843ea469889f779bc16b16b7b766f808a5f1
Cesar Sanz <cesarsanz.91@gmail.com>
2021-09-05 12:37:05 +0200

    Refactor constants

--- src/iotcloud_constants.cpp
+++ src/iotcloud_constants.cpp
@@ -3,24 +3,22 @@
 
 #include "iotcloud_constants.h"
 
-const char IotCloud_Constants::AP_SSID_PREFIX[] = "iot_";
+const char IotCloud_Constants::AP_SSID_PREFIX[] = "iot";
 const char IotCloud_Constants::AP_PASSWORD[] = "12345678";
 const char IotCloud_Constants::MQTT_SERVER[] = "iotcloud.es";
 const char IotCloud_Constants::OTA_SERVER[] = "ota.iotcloud.es";
 const char IotCloud_Constants::NTP_SERVER[] = "pool.ntp.org";
 char IotCloud_Constants::AP_SSID[9] = "";
-char IotCloud_Constants::DEVICE_INTERNAL_ID[4] = "";
+char IotCloud_Constants::DEVICE_INTERNAL_ID[14] = "";
 bool IotCloud_Constants::ENABLE_OTA = true;
 
 void IotCloud_Constants::setup_constants()
 {
-    char buffer[6];
+    byte mac[6];
+    WiFi.macAddress(mac);
     // Set up device ID
-    itoa(ESP.getChipId(), buffer, 6);
-    sprintf(IotCloud_Constants::DEVICE_INTERNAL_ID, "%.*s", 4, buffer + 2);
+    sprintf(IotCloud_Constants::DEVICE_INTERNAL_ID, "%02x%02x%02x%02x-%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
-    // Set up
-    IotCloud_Constants::AP_SSID[0] = '\0';
-    strcat(IotCloud_Constants::AP_SSID, IotCloud_Constants::AP_SSID_PREFIX);
-    strcat(IotCloud_Constants::AP_SSID, IotCloud_Constants::DEVICE_INTERNAL_ID);
+    // Set up config mode Wifi SSID
+    sprintf(IotCloud_Constants::AP_SSID, "%s_%02x%02x", IotCloud_Constants::AP_SSID_PREFIX, mac[4], mac[5]);
 }
